d46dfe4e17ee3a657b27e7a206dc46f0bdf261ed
Jacob3008 <jsneuman2@gmail.com>
2024-02-21 13:17:54 -0500

    Add comments and cleanup

--- A1 Array/main.cpp
+++ A1 Array/main.cpp
@@ -1,3 +1,5 @@
+// Jacob Neuman 2/21/24
+
 #include <iostream>
 using namespace std;
 
@@ -22,11 +24,12 @@ int main() {
 void push(string stack[], string val) {
 	int sp = 0;
 
-	// find top of stack
+	// display error if stack is full
 	if (stack[STACK_SIZE - 1] != "") {
 		cout << "\nerror, overflow" << endl;
 		return;
 	}
+	// find top of stack
 	else {
 		while (stack[sp] != "")
 		{
@@ -42,12 +45,13 @@ void push(string stack[], string val) {
 void pop(string stack[]) {
 	int sp = 0;
 
-	// find top of stack
+	// display error if stack is empty
 	if (stack[0] == "")
 	{
 		cout << "error, underflow" << endl;
 		return;
 	}
+	// find top of the stack
 	else if (stack[STACK_SIZE - 1] != "")
 	{
 		sp = STACK_SIZE - 1;
@@ -68,6 +72,7 @@ void display(string stack[]) {
 	int sp = 0;
 
 	cout << "Stack Elements:";
+	// loop through stack until all elements are displayed
 	while (stack[sp] != "")
 	{
 		cout << " " << stack[sp];
@@ -83,6 +88,7 @@ void validate(string& val) {
 	if (val.length() > 2) {
 		val = val.substr(0, 2);
 	}
+	// add space if stack is 1 character
 	else if (val.length() == 1)
 	{
 		val = val + ' ';
@@ -113,6 +119,7 @@ bool prompt(string stack[]) {
 		cout << "Please enter a value 1-4: "; cin >> choice;
 	}
 
+	// push if choice is 1
 	if (choice == 1) {
 		string val;
 		cout << "Enter value to push to the stack: "; cin >> val;
@@ -120,18 +127,21 @@ bool prompt(string stack[]) {
 		cout << endl;
 		return true;
 	}
+	// pop if choice is 2
 	else if (choice == 2) {
 		cout << endl;
 		pop(stack);
 		cout << endl;
 		return true;
 	}
+	// display if choice is 3
 	else if (choice == 3) {
 		cout << endl;
 		display(stack);
 		cout << endl;
 		return true;
 	}
+	// exit
 	else {
 		return false;
 	}
